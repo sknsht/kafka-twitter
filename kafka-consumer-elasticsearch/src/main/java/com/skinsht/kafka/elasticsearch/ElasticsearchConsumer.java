78285ce75bb2f87aa79c99d8d6d4fc409ca32cf8
sofia <sofiakinsht@gmail.com>
2019-06-02 15:12:21 +0200

    Commit offsets manually

--- kafka-consumer-elasticsearch/src/main/java/com/skinsht/kafka/elasticsearch/ElasticsearchConsumer.java
+++ kafka-consumer-elasticsearch/src/main/java/com/skinsht/kafka/elasticsearch/ElasticsearchConsumer.java
@@ -44,6 +44,7 @@ public class ElasticsearchConsumer {
         while (true) {
             ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
 
+            log.info("Received " + records.count() + " records");
             for (ConsumerRecord<String, String> record : records) {
                 String id = extractIdFromTweet(record.value());
 
@@ -57,10 +58,18 @@ public class ElasticsearchConsumer {
                 IndexResponse indexResponse = client.index(indexRequest, RequestOptions.DEFAULT);
                 log.info(indexResponse.getId());
                 try {
-                    Thread.sleep(1000); // introduce a small delay
+                    Thread.sleep(10); // introduce a small delay
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
+            }
+            log.info("Committing offsets...");
+            consumer.commitSync();
+            log.info("Offsets have been committed");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
         }
     }
@@ -88,6 +97,8 @@ public class ElasticsearchConsumer {
         properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
         properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        properties.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false"); // disable auto commit of offsets
+        properties.setProperty(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "10"); // disable auto commit of offsets
 
         KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
         consumer.subscribe(Arrays.asList(topic));
